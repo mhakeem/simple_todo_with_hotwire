8289b59f13a0fc1ce9b7eece3c2d851589c06a93
mhakeem <mhakeem@users.noreply.github.com>
2025-11-11 20:53:01 -0700

    update and delete task items

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,14 +18,24 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      redirect_to tasks_path, alert: "Error updating task"
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :completed)
   end
 end
