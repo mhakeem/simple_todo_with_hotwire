a4b3ff1ce01ddab7bdb0540a8242fdb27f1e46e0
mhakeem <mhakeem@users.noreply.github.com>
2025-11-12 01:03:03 -0700

    refactor to add turbo stream when adding a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,15 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      redirect_to tasks_path
+      respond_to do |format|
+        format.turbo_stream {
+          render turbo_stream: [
+            turbo_stream.prepend("tasks", partial: "task", locals: { task: @task }),
+            turbo_stream.replace("task_form", partial: "form", locals: { task: Task.new })
+          ]
+        }
+        format.html { redirect_to tasks_path }
+      end
     else
       render :index
     end
