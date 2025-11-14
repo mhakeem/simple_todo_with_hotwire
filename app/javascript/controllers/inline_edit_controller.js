bb3def78d35f09df73c6a4e8940d652d960fe17c
mhakeem <mhakeem@users.noreply.github.com>
2025-11-14 01:32:30 -0700

    add inline edit mode

---
+++ app/javascript/controllers/inline_edit_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="inline-edit"
+export default class extends Controller {
+  static targets = ["display", "form", "input", "deleteButton"]
+
+  edit() {
+    this.displayTarget.classList.add('hidden')
+    this.formTarget.classList.remove('hidden')
+    this.deleteButtonTarget.classList.add('hidden')
+
+    this.inputTarget.focus()
+    this.inputTarget.select()
+  }
+
+  cancel() {
+    this.displayTarget.classList.remove('hidden')
+    this.formTarget.classList.add('hidden')
+    this.deleteButtonTarget.classList.remove('hidden')
+  }
+}
