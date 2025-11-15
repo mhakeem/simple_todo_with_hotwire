5fbf20582344311726e1a308571cfd358e355069
mhakeem <mhakeem@users.noreply.github.com>
2025-11-15 04:04:36 -0700

    add debounce

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,13 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["input", "task", "container"]
+  static values = {
+    delay: { type: Number, default: 300 }
+  }
   
   connect() {
     console.log("Search controller connected!")
     console.log("Found tasks:", this.taskTargets.length)
+    this.timeout = null
   }
   
   filter() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => { this.performFilter() }, this.delayValue)
+  }
+  
+  performFilter() {
     const query = this.inputTarget.value.toLowerCase()
     console.log("Searching for:", query)
     
@@ -26,5 +35,9 @@ export default class extends Controller {
     })
     
     console.log("Visible tasks:", visibleCount)
+  }
+
+  disconnect() {
+    clearTimeout(this.timeout)
   }
 }
