97d65fc274ff453c5002eb8b53b2a9af1a6a3abc
mhakeem <mhakeem@users.noreply.github.com>
2025-11-16 02:18:36 -0700

    Add position field and it's logic in the model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :content, presence: true
+
+  default_scope { order(position: :asc) }
+
+  before_create :set_position
+
+  private
+  def set_position
+    self.position ||= (Task.unscoped.maximum(:position) || 0) + 1
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_15_034050) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_16_073623) do
   create_table "tasks", force: :cascade do |t|
     t.boolean "completed", default: false
     t.text "content"
     t.datetime "created_at", null: false
     t.text "description"
+    t.integer "position"
     t.datetime "updated_at", null: false
   end
 end
