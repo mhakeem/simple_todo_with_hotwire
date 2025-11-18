762809c056ef9260a991a5767e49b86d0688877a
mhakeem <mhakeem@users.noreply.github.com>
2025-11-18 00:47:15 -0700

    switch to esbuild to use react and perform required changes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,20 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import {Application} from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Manually import and register controllers
+import FlashController from "./flash_controller"
+import InlineEditController from "./inline_edit_controller"
+import SearchController from "./search_controller"
+import SortableController from "./sortable_controller"
+import StatusController from "./status_controller"
+import TaskFilterController from "./task_filter_controller"
+
+application.register("flash", FlashController)
+application.register("inline-edit", InlineEditController)
+application.register("search", SearchController)
+application.register("sortable", SortableController)
+application.register("status", StatusController)
+application.register("task-filter", TaskFilterController)
+
+export {application}
